b0d95a5750c1411c44a2f9d97efcf24ea1d20a32
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2025-12-31 15:16:38 +0800

    Create UserResource

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -34,12 +35,12 @@ class User extends Authenticatable
         'is_active' => true,
     ];
 
-    public function assignedAssets()
+    public function assignedAssets(): HasMany
     {
         return $this->hasMany(Asset::class, 'user_id');
     }
 
-    public function assetAssignments()
+    public function assetAssignments(): HasMany
     {
         return $this->hasMany(AssetAssignment::class);
     }
