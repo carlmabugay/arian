cb6a127356e19963f88cb5eecec247510243e6d7
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2026-01-05 18:13:00 +0800

    Update AssignmentsRelationManager on AssetResource

--- app/Enums/AssetStatus.php
+++ app/Enums/AssetStatus.php
@@ -2,7 +2,11 @@
 
 namespace App\Enums;
 
-enum AssetStatus: string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+use Illuminate\Contracts\Support\Htmlable;
+
+enum AssetStatus: string implements HasColor, HasLabel
 {
     case Available = 'available';
     case Assigned = 'assigned';
@@ -13,5 +17,21 @@ enum AssetStatus: string
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::Available => 'success',
+            self::Assigned => 'info',
+            self::Maintenance => 'warning',
+            self::Lost => 'danger',
+            self::Disposed => 'gray',
+        };
+    }
+
+    public function getLabel(): string|Htmlable|null
+    {
+        return $this->name;
     }
 }
--- app/Filament/Resources/Assets/RelationManagers/AssignmentsRelationManager.php
+++ app/Filament/Resources/Assets/RelationManagers/AssignmentsRelationManager.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\Assets\RelationManagers;
 
 use App\Filament\Resources\AssetAssignments\AssetAssignmentResource;
-use Filament\Actions\CreateAction;
+use App\Filament\Resources\Assets\AssetResource;
+use App\Models\User;
+use Filament\Actions\Action;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Enums\Size;
+use Filament\Support\Enums\Width;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use Str;
 
 class AssignmentsRelationManager extends RelationManager
 {
@@ -17,10 +26,51 @@ class AssignmentsRelationManager extends RelationManager
     {
         return $table
             ->heading('Assignment History')
+            ->description(sprintf('Manage %s assignments here.', Str::lower($this->ownerRecord->name)))
             ->headerActions([
-                CreateAction::make()
-                    ->icon('heroicon-o-plus')
-                    ->label('Add New'),
+                Action::make('create')
+                    ->icon(Heroicon::OutlinedPlus)
+                    ->label('Add new')
+                    ->size(Size::Small)
+                    ->slideOver()
+                    ->schema([
+                        Select::make('user_id')
+                            ->relationship('user', 'name')
+                            ->label('Assigned To: ')
+                            ->searchable()
+                            ->options(User::query()->pluck('name', 'id'))
+                            ->required(),
+
+                        Select::make('assigned_by')
+                            ->relationship('assignedBy', 'name')
+                            ->label('Assigned By: ')
+                            ->searchable()
+                            ->options(User::query()->pluck('name', 'id'))
+                            ->required(),
+
+                        DateTimePicker::make('assigned_at')
+                            ->label('Assigned On: ')
+                            ->native(false)
+                            ->required(),
+
+                        DateTimePicker::make('returned_at')
+                            ->label('Return On: ')
+                            ->native(false),
+
+                        Textarea::make('notes')
+                            ->columnSpanFull(),
+                    ])
+                    ->modalHeading(sprintf('Add new %s assignment', $this->ownerRecord->name))
+                    ->modalWidth(Width::Medium)
+                    ->modalFooterActions([
+                        Action::make('create')->submit('create'),
+                        Action::make('cancel')
+                            ->color('gray')
+                            ->url(fn () => AssetResource::getUrl('edit', [
+                                'record' => $this->getOwnerRecord(),
+                            ])),
+                    ])
+                    ->action(fn (array $data) => $this->ownerRecord->assignments()->create($data)),
             ]);
     }
 }
