0557208a7ff23a23f23cd97c7127ac06e196e4d7
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2026-01-19 16:57:57 +0800

    Fix authentication redirection error

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -4,31 +4,16 @@ namespace App\Http\Responses;
 
 use Filament\Auth\Http\Responses\LoginResponse as BaseLoginResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Route;
 use Livewire\Features\SupportRedirects\Redirector;
 
 class LoginResponse extends BaseLoginResponse
 {
     public function toResponse($request): Redirector|RedirectResponse
     {
-        $user = auth()->user();
-
-        // Staff redirect
-        if ($user->isStaff()) {
-            return redirect()->route('filament.admin.resources.assets.index');
+        if (auth()->user()->isStaff()) {
+            return redirect()->to('/admin/assets');
         }
 
-        // SuperAdmin
-        if ($user->isSuperAdmin()) {
-            return redirect()->route('filament.admin.pages.super-admin-dashboard');
-        }
-
-        // CompanyAdmin
-        if ($user->isCompanyAdmin()) {
-            return redirect()->route('filament.admin.pages.company-dashboard');
-        }
-
-        // Default fallback
         return parent::toResponse($request);
     }
 }
